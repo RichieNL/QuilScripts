acc9e5ddac4621f15e28219b6807a7c7dd2d403c
RichieNL <richard@vanveldhuijzenit.nl>
2024-10-31 15:08:52 +0100

    Update quilbuild_installer.sh

--- quilbuild_installer.sh
+++ quilbuild_installer.sh
@@ -7,51 +7,64 @@ SH_FILE="/root/quilbuild.sh"
 read -r -d '' SH_FILE_CONTENT << 'EOL'
 #!/bin/bash
 
-# Display the menu
-echo "Choose an option:"
-echo "1. Firewall configureren voor Cluster Richard"
-echo "2. Firewall configureren voor Cluster Kevin"
-echo "3. Cluster Service voor Worker aanmaken/controleren"
-echo "4. Cluster Service voor Master aanmaken/controleren"
-echo "5. Cluster Service status bekijken"
-echo "6. Token saldo Bekijken" 
-echo "6. Exit"
+# Function to display the menu
+display_menu() {
+    echo "Choose an option:"
+    echo "1. Firewall configureren voor Cluster Richard"
+    echo "2. Firewall configureren voor Cluster Kevin"
+    echo "3. Cluster Service voor Worker aanmaken/controleren"
+    echo "4. Cluster Service status bekijken"
+    echo "5. Token saldo Bekijken"
+    echo "6. Exit"
+}
 
-# Read user choice
-read -p "Enter your choice [1-5]: " choice
+# Main loop to keep the menu running
+while true; do
+    display_menu
 
-# Handle the user choice
-case $choice in
-    1)
-        echo "Downloading Cluster Package..."
-        # Replace the URL with the actual link you want to download
-        wget -O /root/cluster-package.tar.gz http://example.com/cluster-package.tar.gz
-        echo "Download completed and saved to /root/cluster-package.tar.gz"
-        ;;
-    2)
-        echo "Stopping Cluster..."
-        # Place your stop cluster commands here
-        ;;
-    3)
-        echo "Checking Cluster Status..."
-        # Place your check status commands here
-        ;;
-    4)
-        echo "Updating Cluster Configuration..."
-        # Place your update configuration commands here
-        ;;
-    5)
-        echo "Exiting..."
-        exit 0
-        ;;
-     6)
-        echo "Exiting..."
-        exit 0
-        ;;
-    *)
-        echo "Invalid choice. Please select a number between 1 and 5."
-        ;;
-esac
+    # Read user choice
+    read -p "Enter your choice [1-6]: " choice
+
+    # Handle the user choice
+    case $choice in
+        1)
+            echo "Downloading Cluster Package..."
+            # Replace the URL with the actual link you want to download
+            wget -O /root/cluster-package.tar.gz http://example.com/cluster-package.tar.gz
+            echo "Download completed and saved to /root/cluster-package.tar.gz"
+            
+            # Ask if the user wants to return to the menu
+            read -p "Do you want to return to the menu? (y/n): " return_choice
+            if [[ "$return_choice" != "y" && "$return_choice" != "Y" ]]; then
+                echo "Exiting..."
+                exit 0
+            fi
+            ;;
+        2)
+            echo "Configuring Firewall for Cluster Kevin..."
+            # Place your firewall configuration commands here for Cluster Kevin
+            ;;
+        3)
+            echo "Creating/Checking Cluster Service for Worker..."
+            # Place your service creation/check commands here for Worker
+            ;;
+        4)
+            echo "Viewing Cluster Service Status..."
+            # Place your cluster service status check commands here
+            ;;
+        5)
+            echo "Viewing Token Balance..."
+            # Place your token balance view commands here
+            ;;
+        6)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please select a number between 1 and 6."
+            ;;
+    esac
+done
 EOL
 
 # Create the .sh file with the specified content
